7814ae6a61f210d45b2256fc92cec7c94914604e
Richard Lemmond <rglemmond86@yahoo.com>
2023-06-09 18:56:58 -0400

    WIP correects value priority

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ function substituteNumber(text) {
 
   const substitutedValues = [];
   values.forEach((value) => {
-    if (value.includes("1")) {
-      substitutedValues.push("Beep!");
+    if (value.includes("3")) {
+      substitutedValues.push("Won't you be my neighbor?");
     } else if (value.includes("2")) {
       substitutedValues.push("Boop!");
-    } else if (value.includes("3")) {
-      substitutedValues.push("Won't you be my neighbor?");
+    } else if (value.includes("1")) {
+      substitutedValues.push("Beep!");
     } else {
       substitutedValues.push(value);
     }
